99700d13507de6f0b0308e0217be8ff811e09607
YuriiAlekseev <ash030788@gmail.com>
2022-11-30 22:27:27 +0300

    Домашняя работа задача 4

---
+++ HomeWork_task4/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Clear();
+Console.Write("Введите первое число: ");
+int first_number = int.Parse(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int second_number = int.Parse(Console.ReadLine());
+Console.Write("Введите третье число: ");
+int third_number = int.Parse(Console.ReadLine());
+int max = first_number;
+if (second_number > max)
+{
+max = second_number;
+if (third_number > max)
+{
+max = third_number;
+}
+}
+Console.WriteLine($"Максимальное из введенных чисел является число: {max}");
