1362a8359729ef3c1045ed844c7ef405bcf58922
YuriiAlekseev <ash030788@gmail.com>
2022-11-30 21:48:59 +0300

    Задача номер 2

---
+++ HomeWork_task2/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+Console.Write("Введите первое число: ");
+int first_number = int.Parse(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int second_number = int.Parse(Console.ReadLine());
+if (first_number > second_number)
+{
+    Console.WriteLine($"Число {first_number} больше, чем число {second_number}");
+}
+else 
+{
+    Console.WriteLine($"Число {second_number} больше, чем число {first_number}");
+}
