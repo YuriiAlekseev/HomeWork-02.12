784866816f34cb76056e58c914551c23b01b86fa
YuriiAlekseev <ash030788@gmail.com>
2022-12-01 18:36:54 +0300

    Задача номер 8

---
+++ HomeWork_task6/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Clear();
+Console.WriteLine("Введите число: ");
+int number = int.Parse (Console.ReadLine());
+if (0 == number % 2)
+{
+    Console.WriteLine("Данное число является четным");
+}
+else
+{
+    Console.WriteLine("Данное число является не четным");
+}
